aa248ea72f7512aa9e609b4db3a6d2fa42a8c4c7
EC2 Default User <ec2-user@ip-172-31-25-82.eu-north-1.compute.internal>
2026-05-29 07:06:47 +0000

    If else Conditions

---
+++ day02/check_jethalal_loyal.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+read -p "Jetha's love  is : " wife
+read -p "Jetha;s love % : " love
+if [[ $wife == " Daya bhabhi " ]];
+then
+	echo "Jetha is loyal"
+
+elif [[ $love -ge 100 ]];
+then
+	echo "Jetha is loyal" 
+else
+	echo "Jetha is not loyal"
+fi
